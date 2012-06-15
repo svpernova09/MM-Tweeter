694976850db2d77ddbba26f77080d59722a2667c
Svpernova09 <halo09@gmail.com>
2012-06-15 12:14:06 -0500

    Cleanup of some undefined errors happening when no $_POST/$_GET data present (mainly fix for CLI cron runs)

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 global $debug;
-$debug = 1;
+$debug = 0;
 if($debug){
 	ini_set ('display_errors', 1);
 	error_reporting (E_ALL & ~E_NOTICE);
@@ -19,6 +19,19 @@ if(isset($_POST['secret'])){
 	$secret = trim($_POST['secret']);
 	$added = '';
 	checkpending($source,$secret);
+	//$validsource = checksecret('self','c0:36:48:b7:a5:76:ae:3d:ee:9c:da:fd:8c:2c:42:0d');
+	$validsource = checksecret($source,$secret);
+	$validpost_date = checkpost_date($post_date);
+	if ($validsource || $validpost_date){
+		if($debug){ echo("Source is valid and post_date is valid<BR />");}
+		if ($content == ''){
+			if($debug){ echo("Content is empty / blank<BR />");}
+		} else {
+			if($debug){ echo("Content isn't empty. Add to pending.<BR />");}
+			$added_pending = addpending($source,$secret,$post_date, $content);	
+		}
+	}
+
 }
 if(isset($_GET['secret'])){
 	$source = trim($_GET['source']);
@@ -27,20 +40,21 @@ if(isset($_GET['secret'])){
 	$secret = trim($_GET['secret']);
 	$added = '';
 	checkpending($source,$secret);
-} else{
-	echo("<p>You Shouldn't be here. If you think you should, you did something wrong to get here</p>");
-	checkpending(demo,dummy);
-}
-//$validsource = checksecret('self','c0:36:48:b7:a5:76:ae:3d:ee:9c:da:fd:8c:2c:42:0d');
-$validsource = checksecret($source,$secret);
-$validpost_date = checkpost_date($post_date);
-if ($validsource || $validpost_date){
-	if($debug){ echo("Source is valid and post_date is valid<BR />");}
-	if ($content == ''){
-		if($debug){ echo("Content is empty / blank<BR />");}
-	} else {
-		if($debug){ echo("Content isn't empty. Add to pending.<BR />");}
-		$added_pending = addpending($source,$secret,$post_date, $content);	
+	//$validsource = checksecret('self','c0:36:48:b7:a5:76:ae:3d:ee:9c:da:fd:8c:2c:42:0d');
+	$validsource = checksecret($source,$secret);
+	$validpost_date = checkpost_date($post_date);
+	if ($validsource || $validpost_date){
+		if($debug){ echo("Source is valid and post_date is valid<BR />");}
+		if ($content == ''){
+			if($debug){ echo("Content is empty / blank<BR />");}
+		} else {
+			if($debug){ echo("Content isn't empty. Add to pending.<BR />");}
+			$added_pending = addpending($source,$secret,$post_date, $content);	
+		}
 	}
+} else{
+	//echo("<p>You Shouldn't be here. If you think you should, you did something wrong to get here</p>");
+	checkpending("demo","dummy");
 }
+
 ?>
